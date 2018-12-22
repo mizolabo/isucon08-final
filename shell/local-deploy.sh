3e96e888c911307b1cf3c648c961d5e4d6075ad8
hhayashi <hayashi@vesuvio-system.com>
2018-12-22 16:23:40 +0900

    ローカル用デプロイシェルにblackboxの起動を追加

--- shell/local-deploy.sh
+++ shell/local-deploy.sh
@@ -3,12 +3,18 @@
 set -eu
 
 # docker
-echo 'docker-compose start...'
+echo 'docker-compose webapp start...'
 docker-compose -f /home/vagrant/isucon08-final/webapp/docker-compose.yml -f /home/vagrant/isucon08-final/webapp/docker-compose.go.yml down
 # docker-compose -f /home/vagrant/isucon08-final/webapp/docker-compose.yml -f /home/vagrant/isucon08-final/webapp/docker-compose.go.yml build
 docker-compose -f /home/vagrant/isucon08-final/webapp/docker-compose.yml -f /home/vagrant/isucon08-final/webapp/docker-compose.go.yml up -d
-echo 'docker-compose end...'
+echo 'docker-compose webapp end...'
 
+# blackbox
+echo 'docker-compose blackbox start...'
+docker-compose -f /home/vagrant/isucon08-final/blackbox/docker-compose.local.yml down
+# docker-compose -f /home/vagrant/isucon08-final/blackbox/docker-compose.local.yml build
+docker-compose -f /home/vagrant/isucon08-final/blackbox/docker-compose.local.yml up -d
+echo 'docker-compose blackbox end...'
 
 # echo 'bench make start...'
 # cd /home/vagrant/isucon08-final/bench
